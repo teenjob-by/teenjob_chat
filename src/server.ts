c32c41dee41af92d63837de897a1f957ce018bb6
Siarhei <zykau_s@mail.ru>
2020-06-13 13:41:05 +0300

    feat: added sosket connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import * as express from "express";
+import * as socketIO from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+
+const io = socketIO(createServer(app));
+
+io.on("connect", (socket) => {
+  console.log("Connected");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
