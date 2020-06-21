a92b167f3b9791f337451eac87292e2f4d1e1620
kabolov <alexander.kabolov@gmail.com>
2020-06-21 17:58:26 +0300

    main methods implemented

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as socketIO from "socket.io";
 import { createServer } from "http";
 import { pool } from "./database";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const server = createServer(app);
@@ -10,23 +11,52 @@ app.use(express.json());
 
 const io = socketIO(server);
 
-app.get("/", async () => {
-  await pool.query("INSERT INTO users (user_id, name) VALUES ($1, $2)", [
-    "123123123",
-    "sasha",
+const handleUser = async (userId, userName) => {
+  const res = await pool.query("SELECT * FROM users WHERE user_id = $1", [
+    userId,
   ]);
-});
 
-const createUser = (userId) => {
-  //create user
+  if (res.rowCount === 0) {
+    pool.query("INSERT INTO users (user_id, name) VALUES ($1, $2)", [
+      userId,
+      userName,
+    ]);
+  }
 };
 
-const joinToRoom = (socket, userId) => (interlocutorId) => {
-  //join users to the  room
+const joinToRoom = (socket, userId) => async (interlocutorId) => {
+  const room = await pool.query(
+    "SELECT * FROM rooms WHERE (user1 = $1 and user2 = $2) OR (user1 = $2 and user2 = $1) limit 1",
+    [userId, interlocutorId]
+  );
+
+  const roomId = room.rows[0].id || uuidv4();
+
+  if (room.rowCount === 0) {
+    await pool.query(
+      "INSERT INTO rooms (id, user1, user2) VALUES ($1, $2, $3)",
+      [roomId, userId, interlocutorId]
+    );
+
+    await pool.query(
+      "CREATE TABLE $1(message VARCHAR(255), from VARCHAR(255), time TIMESTAMP)",
+      [roomId]
+    );
+  }
+
+  socket.join(roomId);
 };
 
-const sendMessage = (socket) => (room, msg) => {
-  //send message to users
+const sendMessage = (socket) => async (roomId, msg) => {
+  await pool.query("INSERT INTO $1 (message, from) VALUES($2, $3)", [
+    roomId,
+    msg.content,
+    msg.from,
+  ]);
+
+  const room = await pool.query("SELECT * FROM $1", [roomId]);
+
+  socket.to(roomId).emit("getMessage", msg);
 };
 
 io.on("connection", (socket) => {
@@ -35,7 +65,7 @@ io.on("connection", (socket) => {
   socket.send("You are connected successfully.");
   const userId = socket.handshake.query.id;
 
-  createUser(userId);
+  handleUser(userId, "jora");
 
   socket.on("connectToUser", joinToRoom(socket, userId));
 
